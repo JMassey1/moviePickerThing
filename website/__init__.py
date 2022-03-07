8ddbef643e2d5dbb55fbda16afcac6cf58cf10cd
Jordan Massey <jmmhqw@mst.edu>
2022-03-07 15:59:02 -0600

    made it so secret key is changed and stored in heroku, no longer plain text git

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'EoX;)#xk9X&ApMPBJ0X,#@"pM+aB@W'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
