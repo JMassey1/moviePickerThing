7f8a3f4c83fcbddcbee341c6183e1a9a217ff36b
Jordan Massey <j.massey2278@gmail.com>
2022-03-10 14:48:32 -0600

    trying to fix default sorting

--- website/static/index.js
+++ website/static/index.js
@@ -6,8 +6,8 @@ $('#movieTable').DataTable( {
         {'visible': false, 'targets':[4,5], 'searchable': false},
         {'orderData': [4], 'targets':[3]},
         {'orderData': [5], 'targets':[2]},
-        {'order': [[2, 'dec']]}
     ],
+    'order': [[2, 'dec']],
     stateSave: true
 });
 
@@ -22,8 +22,8 @@ $('#songTable').DataTable( {
         {'visible': false, 'targets':[5,6], 'searchable': false},
         {'orderData': [5], 'targets':[4]},
         {'orderData': [6], 'targets':[3]},
-        {'order': [[3, 'dec']]}
     ],
+    'order': [[3, 'dec']],
     stateSave: true
 })
 })
